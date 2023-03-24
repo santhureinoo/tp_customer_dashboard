db1460d40bb058340955b31778324c4380624ed6
sithutin1997 <sithu5897@gmail.com>
2023-03-24 14:57:29 +0630

    changed backend url

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -52,7 +52,7 @@ export default withIronSessionApiRoute(
         `;
 
         const client = new ApolloClient({
-            link: new HttpLink({ uri: 'http://18.141.128.98:4000/', fetch }),
+            link: new HttpLink({ uri: 'http://localhost:4000', fetch }),
             cache: new InMemoryCache(),
         });
 
