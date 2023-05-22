935e5e1950e75f3adf9327555417e956b931da75
San_ThureinOo <santhueinoo@gmail.com>
2023-05-22 14:19:22 +0630

    fix: CSS for widgets

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -12,7 +12,7 @@ import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Dashboard = ({ groupId }: any): JSX.Element => {
     const [currentCustomerID, setCurrentCustomerID] = React.useState(1);
-    const [currentPage, setCurrentPage] = React.useState<String>('outlet');
+    const [currentPage, setCurrentPage] = React.useState<String>('summary');
     const [lastestLiveDate, setLastestLiveDate] = React.useState('');
     const [outlets, setOutlets] = React.useState<outlet[]>([]);
     const [currentOutlet, setCurrentOutlet] = React.useState<outlet>();
@@ -621,7 +621,7 @@ const Dashboard = ({ groupId }: any): JSX.Element => {
                              */
                         }
                         <div className="flex gap-5 justify-between">
-                            <YearlyEnergyCard Svg="/asserts/energy-icon.png" Prefix="$" Value={numberWithCommas(summaryResults.energySaving)} Postfix="Energy" Year="Saved / Year" BgColor="bg-blue-200" TextColor="text-custom-blue-card-font" Height="90" Width="90" />
+                            <YearlyEnergyCard Svg="/asserts/energy-icon.png" Value={`$` + numberWithCommas(summaryResults.energySaving)} Postfix="Energy" Year="Saved / Year" BgColor="bg-blue-200" TextColor="text-custom-blue-card-font" Height="90" Width="90" />
                             <YearlyEnergyCard Svg="/asserts/carbondioxide.png" Value={numberWithCommas(summaryResults.co2Saving)} Postfix="Kg CO" SmallPostfix="2" Year="Saved / Year" BgColor="bg-gray-200" TextColor="text-custom-gray" Height="150" Width="150" />
                             <YearlyEnergyCard Svg="/asserts/bigtree.png" Value={numberWithCommas(Math.round(summaryResults.co2Saving / 22))} Postfix="Trees" Year="to be planted / Year" BgColor="bg-green-200" TextColor="text-custom-green-card-font" Height="150" Width="150" />
                             <YearlyEnergyCard Svg="/asserts/meals.png" Value={numberWithCommas(summaryResults.co2Saving * 2)} Postfix="Meals" Year="to be sold / Year" BgColor="bg-orange-200" TextColor="text-custom-orange-card-font" Height="150" Width="150" />
